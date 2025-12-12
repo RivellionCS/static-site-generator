58fa21d4a4f4117e609c23e1d1a80db00e323ca1
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-12-11 23:40:00 -0400

    add function for extracting images/links from markdown and tests

---
+++ src/test_extract_markdown_links.py
@@ -0,0 +1,21 @@
+import unittest
+from extract_markdown_links import extract_markdown_links
+
+class test_extract_markdown_links(unittest.TestCase):
+    def test_one_link(self):
+        matches = extract_markdown_links(
+            "This is a link to [google](https://www.google.com)"
+        )
+        self.assertEqual([("google", "https://www.google.com")], matches)
+
+    def test_image_and_link(self):
+        matches = extract_markdown_links(
+            "This is a [link](https://www.google.com) and ![image](https://www.images.com/image.png)"
+        )
+        self.assertEqual([("link", "https://www.google.com")], matches)
+
+    def text_multiple_links(self):
+        matches = extract_markdown_links(
+            "This is one [link](https://www.google.com) and this is [another](https://www.brave.com)"
+        )
+        self.assertEqual([("link", "https://www.google.com"), ("another", "https://www.brave.com")], matches)
---
+++ src/text_extract_markdown_images.py
@@ -0,0 +1,21 @@
+import unittest
+from extract_markdown_images import extract_markdown_images
+
+class text_extract_markdown_images(unittest.TestCase):
+    def test_one_image(self):
+        matches = extract_markdown_images(
+            "This is a text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_image_and_link(self):
+        matches = extract_markdown_images(
+            "This is a [link](https://www.google.com) and ![image](https://www.images.com/image.png)"
+        )
+        self.assertEqual([("image", "https://www.images.com/image.png")], matches)
+
+    def test_multiple_images(self):
+        matches = extract_markdown_images(
+            "This is one [image](https://www.images.com/image01.png) and two [image1](https://www.images.com/image02.png)"
+        )
+        self.assertEqual([("image", "https://www.images.com/image01.png"), ("image1", "https://www.images.com/image02.png")], matches)
