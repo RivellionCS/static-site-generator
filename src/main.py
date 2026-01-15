8273854019d56e9f2ad13fbbdfddd5892079803d
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-01-15 02:41:36 -0400

    create function to recursivly write files/folders

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
-from textnode import TextNode, TextType
+import os, shutil
+
+def copy_static_to_public():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+
+    recursive_copy("static", "public")
+
+def recursive_copy(source, destination):
+    directory_list = os.listdir(source)
+    for item in directory_list:
+        item_source_path = os.path.join(source, item)
+        item_destination_path = os.path.join(destination, item)
+        if os.path.isfile(item_source_path):
+            shutil.copy(item_source_path, item_destination_path)
+        else:
+            os.mkdir(item_destination_path)
+            recursive_copy(item_source_path, item_destination_path)
+    
+
 
 def main():
-    test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(test)
-    print("hello")
+    copy_static_to_public()
 
 main()
